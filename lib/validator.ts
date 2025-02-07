c6ae0c87f4eb4dfb749c4d1cf8d1b50fa0c5efc0
Amirhoseinmiri <amirhoseinmiri444@gmail.com>
2025-02-07 06:07:02 +0330

    handle middleware size

--- lib/validator.ts
+++ lib/validator.ts
@@ -64,3 +64,14 @@ export const insertCartSchema = z.object({
     .min(1, "Session Cart ID must be at least 3 characters"),
   userId: z.string().optional().nullable(),
 });
+
+// schema for shipping address
+export const shippingAddressSchema = z.object({
+  fullName: z.string().min(3, "Full name must be at least 3 characters"),
+  streetAddress: z.string().min(3, "Address must be at least 3 characters"),
+  city: z.string().min(3, "City must be at least 3 characters"),
+  postalCode: z.string().min(3, "Postal code must be at least 3 characters"),
+  country: z.string().min(3, "Country must be at least 3 characters"),
+  lat: z.number().optional().nullable(),
+  lng: z.number().optional().nullable(),
+});
